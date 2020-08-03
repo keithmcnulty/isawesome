7d30e75db8266368aa676f0e6d805f7ca3d1a2c7
Keith McNulty <keith.mcnulty@gmail.com>
2020-08-03 12:26:39 +0100

    Adjust isawesome

--- R/isawesome.R
+++ R/isawesome.R
@@ -5,19 +5,29 @@
 #'
 #' @param name A character string of the person or object to be complimented
 #' @param alternative A logical indicating whether alternatives to 'is awesome' should be used
+#' @param return_message A logical indicating whether a message should be returned.  If FALSE a character string will be returned.
 #' @return A message that pays `name` a compliment.
 #'
 #' @examples
 #' isawesome("Randy 'Macho Man' Savage")
+#' isawesome("Hulk Hogan", alternative = TRUE)
 
 # this function will be exported
-isawesome <- function(name = "Someone", alternative = FALSE) {
-  if (alternative) {
-    paste(name, "is awesome!") %>%
-      message()
+isawesome <- function(name = "Someone", alternative = FALSE, return_message = TRUE) {
+  if (!alternative) {
+    if (return_message) {
+      paste(name, "is awesome!") %>%
+        message()
+    } else {
+      paste(name, "is awesome!")
+    }
   } else {
-    paste(name, .random_compliment()) %>%
-      message()
+    if (return_message) {
+      paste(name, .random_compliment()) %>%
+        message()
+    } else {
+      paste(name, .random_compliment())
+    }
   }
 }
 
