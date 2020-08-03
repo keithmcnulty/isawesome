d6fbb5bb9b547db806032480c018beeaf88478e9
Keith McNulty <keith.mcnulty@gmail.com>
2020-08-03 12:30:40 +0100

    Adjust isawesome()

--- R/isawesome.R
+++ R/isawesome.R
@@ -1,12 +1,12 @@
 #' Gives compliments
 #'
 #' @description
-#' `isawesome()` displays a message that compliments the input.
+#' Displays a message that compliments the input.
 #'
 #' @param name A character string of the person or object to be complimented
 #' @param alternative A logical indicating whether alternatives to 'is awesome' should be used
 #' @param return_message A logical indicating whether a message should be returned.  If FALSE a character string will be returned.
-#' @return A message that pays `name` a compliment.
+#' @return A message that pays a compliment.
 #'
 #' @examples
 #' isawesome("Randy 'Macho Man' Savage")
